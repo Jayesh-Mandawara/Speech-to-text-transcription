062d055cd24d8a430800a9ed342aa77b8bc08635
Jayesh Mandawara <150209576+Jayesh-Mandawara@users.noreply.github.com>
2024-02-29 23:08:06 +0530

    Add files via upload

---
+++ languages.js
@@ -0,0 +1,31 @@
+const languages = [
+  {
+    no: "1",
+    name: "English",
+    native: "English",
+    code: "en",
+  },
+  {no:"2",
+  name: "Bengali",
+  native: "বাংলা",
+  code: "bn-IN"
+  },
+  {no:"3",
+  name:"Punjabi",
+  native: "ਪੰਜਾਬੀ",
+  code: "pa-IN"
+  },
+  {
+    no: "4",
+    name: "Hindi",
+    native: "हिन्दी",
+    code: "hi",
+  },
+  
+  {
+    no: "55",
+    name: "Urdu",
+    native: "اردو",
+    code: "ur",
+  },
+];
---
+++ script.js
@@ -0,0 +1,80 @@
+const recordBtn = document.querySelector(".record"),
+  result = document.querySelector(".result"),
+  inputLanguage = document.querySelector("#language"),
+  clearBtn = document.querySelector(".clear");
+
+let SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition,
+  recognition,
+  recording = false;
+
+function populateLanguages() {
+  languages.forEach((lang) => {
+    const option = document.createElement("option");
+    option.value = lang.code;
+    option.innerHTML = lang.name;
+    inputLanguage.appendChild(option);
+  });
+}
+
+populateLanguages();
+
+function speechToText() {
+  try {
+    recognition = new SpeechRecognition();
+    recognition.lang = inputLanguage.value;
+    recognition.interimResults = true;
+    recordBtn.classList.add("recording");
+    recordBtn.querySelector("p").innerHTML = "Listening...";
+    recognition.start();
+    recognition.onresult = (event) => {
+      const speechResult = event.results[0][0].transcript;
+      if (event.results[0].isFinal) {
+        result.innerHTML += " " + speechResult;
+        result.querySelector("p").remove();
+      }
+    };
+    recognition.onspeechend = () => {
+      speechToText();
+    };
+    recognition.onerror = (event) => {
+      stopRecording();
+      if (event.error === "no-speech") {
+        alert("No speech was detected. Stopping...");
+      } else if (event.error === "audio-capture") {
+        alert(
+          "No microphone was found. Ensure that a microphone is installed."
+        );
+      } else if (event.error === "not-allowed") {
+        alert("Permission to use microphone is blocked.");
+      } else if (event.error === "aborted") {
+      } else {
+        alert("Error occurred in recognition: " + event.error);
+      }
+    };
+  } catch (error) {
+    recording = false;
+
+    console.log(error);
+  }
+}
+
+recordBtn.addEventListener("click", () => {
+  if (!recording) {
+    speechToText();
+    recording = true;
+  } else {
+    stopRecording();
+  }
+});
+
+function stopRecording() {
+  recognition.stop();
+  recordBtn.querySelector("p").innerHTML = "Start Listening";
+  recordBtn.classList.remove("recording");
+  recording = false;
+}
+
+clearBtn.addEventListener("click", () => {
+  result.innerHTML = "";
+});
